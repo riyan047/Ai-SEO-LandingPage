2ad764121954629f6db996e2eb331e534b3b483d
Riyan <griyan115@gmail.com>
2025-04-24 12:04:59 +0530

    added logoTicker animation

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { plugin } from 'postcss';
 import apexLogo from '../assets/logo-apex.png';
 import acmeLogo from '../assets/logo-acme.png';
@@ -5,6 +6,7 @@ import celestialLogo from '../assets/logo-celestial.png';
 import quantumLogo from '../assets/logo-quantum.png';
 import pulseLogo from '../assets/logo-pulse.png';
 import echoLogo from '../assets/logo-echo.png';
+import {motion} from 'framer-motion'
 
 export const LogoTicker = () => {
   return (
@@ -14,12 +16,22 @@ export const LogoTicker = () => {
         <div className='flex-1 md:flex-none'>
           <h2>Trusted by top innovative teams</h2>
         </div>
-        <div className='flex-1 md:flex-none overflow-hidden w-28 md:w-[465px] lg:w-[777px] [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]'>
-          <div className='flex flex-none gap-14'>
-            {[apexLogo, acmeLogo, celestialLogo, quantumLogo, pulseLogo, echoLogo].map((logo) => (
+        <div className=' flex flex-1 md:flex-none overflow-hidden w-28 md:w-[465px] lg:w-[777px] [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]'>
+          <motion.div 
+          initial={{translateX:'-50%'}}
+          animate={{
+            translateX:'0%'
+          }}
+          transition={{
+            duration:30,
+            repeat:Infinity,
+            ease:"linear"
+          }}
+          className='flex flex-none gap-14 pr-14 '>
+              {[apexLogo, acmeLogo, celestialLogo, quantumLogo, pulseLogo, echoLogo, apexLogo, acmeLogo, celestialLogo, quantumLogo, pulseLogo, echoLogo].map((logo) => (
               <img src={logo.src} key={logo.src} className='h-6 w-auto' />
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
