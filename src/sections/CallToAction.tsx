276f34fcf6f0676b575a8c5cdfe249b1f3b4fef3
Riyan <griyan115@gmail.com>
2025-04-25 22:05:51 +0530

    callToAction stars animation

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,20 +1,46 @@
+"use client";
 import { Button } from "@/components/Button";
 import starsBg from '@/assets/stars.png';
-import gridLines from '@/assets/grid-lines.png'
+import gridLines from '@/assets/grid-lines.png';
+import { motion, animate, useTransform, useScroll } from 'framer-motion';
+import { useRef } from "react";
 
 export const CallToAction = () => {
+
+  const sectionRef = useRef<HTMLElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"]
+  });
+
+  const backgroundPositionY = useTransform(scrollYProgress, [0, 1], [-300, 300]);
   return (
-    <section className="py-20 md:py-24">
+    <section
+
+      ref={sectionRef} className="py-20 md:py-24">
       <div className="container ">
-        <div className="border border-white/15 py-24 rounded-xl overflow-hidden relative"
+        <motion.div className="border border-white/15 py-24 rounded-xl overflow-hidden relative"
           style={{
-            backgroundImage: `url(${starsBg.src})`
+            backgroundImage: `url(${starsBg.src})`,
+            backgroundPositionY
+          }}
+          animate={{
+            backgroundPositionX: starsBg.width
+          }}
+          transition={{
+            duration: 120,
+            repeat: Infinity,
+            ease: "linear"
           }}
         >
-          <div className="absolute inset-0 bg-[rgba(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)]"
+          <div
+
+            className="absolute inset-0 bg-[rgba(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)]"
             style={{
               backgroundImage: `url(${gridLines.src})`
             }}
+
           ></div>
           <div className="relative">
             <h2 className="text-5xl md:text-6xl max-w-sm mx-auto tracking-tighter text-center font-medium">AI-driven SEO for everyone.</h2>
@@ -23,8 +49,8 @@ export const CallToAction = () => {
               <Button>Join waitlist</Button>
             </div>
           </div>
-        </div>
-      </div>  
+        </motion.div>
+      </div>
     </section>
   )
 };
