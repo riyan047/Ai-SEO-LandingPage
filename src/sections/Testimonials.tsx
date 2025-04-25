a249344f423fbd6ccc4d6e1c00ca14bc60815a00
Riyan <griyan115@gmail.com>
2025-04-25 19:11:28 +0530

    added testimonials animation

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,8 +1,10 @@
+"use client"
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
 import avatar4 from "@/assets/avatar-4.png";
 import Image from "next/image";
+import { motion } from "framer-motion"
 
 const testimonials = [
   {
@@ -39,9 +41,21 @@ export const Testimonials = () => {
         <p className="text-lg md:text-xl text-white/70 text-center mt-5 tracking-tight max-w-sm mx-auto">
           Our revolutionary AI SEO tools have transformed our clients' strategies.
         </p>
-        <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-          <div className="flex gap-5">
-            {testimonials.map(testimonial => (
+        <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <motion.div
+            initial={{
+              translateX:"-50%"
+            }}
+            animate={{
+              translateX:"0"
+            }}
+            transition={{
+              duration:30,
+              ease:"linear",
+              repeat:Infinity
+            }}
+            className="flex gap-5 pr-5 flex-none ">
+            {[...testimonials, ...testimonials].map(testimonial => (
               <div key={testimonial.name} className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,0.3),black)] max-w-xs md:max-w-md flex-none">
                 <div className="text-lg tracking-tight md:text-2xl">{testimonial.text}</div>
                 <div className="flex items-center gap-3 mt-5 ">
@@ -59,7 +73,7 @@ export const Testimonials = () => {
                 </div>
               </div>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
