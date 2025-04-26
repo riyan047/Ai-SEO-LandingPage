f614dc911f137d999cc9f0d6ccc667f3cf8ff185
Riyan <griyan115@gmail.com>
2025-04-26 19:07:19 +0530

    ui fixes

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -44,7 +44,7 @@ export const CallToAction = () => {
 
   const [mouseX, mouseY] = useRelativeMousePosition(borderedDivRef)
 
-  const maskImage= useMotionTemplate`radial-gradient(25% 50% at ${mouseX}px ${mouseY}px, black, transparent)`
+  const maskImage= useMotionTemplate`radial-gradient(50% 50% at ${mouseX}px ${mouseY}px, black, transparent)`
   return (
     <section
       ref={sectionRef} className="py-20 md:py-24">
